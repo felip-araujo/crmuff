29624fb12be10e440dcaf2efeef7f6083f8c3bea
felip-araujo <felipearaujoexecutive@gmail.com>
2025-08-19 10:32:28 -0400

    pre-cadastro

--- assets/js/login.js
+++ assets/js/login.js
@@ -48,3 +48,47 @@ async function login() {
     error.classList.remove("hidden");
   }
 }
+
+async function preCadastro() {
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("e-mail").value;
+  const departamento = document.getElementById("departamento").value;
+  const re = document.getElementById("re").value;
+  const senha1 = document.getElementById("senha1").value;
+  const senha2 = document.getElementById("senha-confirm").value;
+  const error = document.getElementById("errorModal");
+  const sucess = document.getElementById("sucessModal");
+
+  const senhaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=\[\]{};:'",.<>/?\\|`~]).{8,}$/;
+
+  if (senha1 !== senha2) {
+    error.textContent = "As senhas não coincidem";
+    error.classList.remove("hidden");
+    return;
+  }
+
+  if (!senhaRegex.test(senha2)) {
+    error.textContent = "A senha deve ter no mínimo 8 caracteres, incluindo maiúscula, minúscula, número e caractere especial.";
+    error.classList.remove("hidden");
+    return;
+  }
+
+  try {
+    const response = await fetch("https://evoludesign.com.br/api-conipa/auth/pre-cadastro.php", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ nome, email, departamento, re, senha: senha2 })
+    });
+
+    if (!response.ok) {
+      throw new Error("Erro ao realizar o Pré-Cadastro");
+    } else {
+      sucess.textContent = "Pré-Cadastro Realizado, aguarde aprovação de um administrador!";
+      sucess.classList.remove("hidden");
+    }
+  } catch (err) {
+    console.error("erro", err);
+  }
+
+}
+
